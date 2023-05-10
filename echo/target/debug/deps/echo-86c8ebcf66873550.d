f030c4799d32d8b6a973481432419cb1c8145c8b
Igor <igor@devfire.io>
2023-05-10 11:21:23 -0400

    .gitignore is now working

--- echo/target/debug/deps/echo-86c8ebcf66873550.d
+++
@@ -1,7 +0,0 @@
-/home/ig/Documents/rust/gossip-glomers/echo/target/debug/deps/echo-86c8ebcf66873550.rmeta: src/main.rs
-
-/home/ig/Documents/rust/gossip-glomers/echo/target/debug/deps/echo-86c8ebcf66873550.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
