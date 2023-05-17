9b2bced2d89fd73969842a70fdbb6086bbd2ac32
Igor <igor@devfire.io>
2023-05-17 16:02:23 -0400

    challenge 3b WIP

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::{BufRead, Write}, collections::{HashSet, HashMap}};
+use std::{
+    collections::{HashMap, HashSet},
+    io::{BufRead, Write},
+};
 
 use gossip_glomers::protocol::{Body, Message, Payload};
 
@@ -29,6 +32,9 @@ fn main() -> anyhow::Result<()> {
     // messages we know the neighbors have received
     let mut confirmed_received: HashMap<String, HashSet<usize>>;
 
+    // the list of nodes we should gossip with
+    let mut neighborhood: Vec<String>;
+
     // Use the lines iterator from the io::BufRead trait.
     // This iterator yields lines from a buffer, where each line is terminated by a newline character
     for (generated_msg_id, line) in stdin.lock().lines().enumerate() {
@@ -37,7 +43,14 @@ fn main() -> anyhow::Result<()> {
             // This function takes a string and deserializes it into a struct.
             let input: Message = serde_json::from_str(&line)?;
             match input.body.payload {
-                Payload::Topology { .. } => {
+                Payload::Topology { mut topology } => {
+
+                    // Removes a key from the map, returning the value at the key if the key was previously in the map.
+                    // We need this to figure out who our neighbors are.
+                    neighborhood = topology
+                        .remove(&input.dest)
+                        .unwrap_or_else(|| panic!("Failed extracting our hood from topology"));
+
                     // ack it
                     let reply = Message {
                         src: input.dest,
@@ -65,6 +78,8 @@ fn main() -> anyhow::Result<()> {
                         },
                     };
                     send_reply(reply)?;
+
+
                 }
 
                 Payload::Read => {
